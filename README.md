e1f1b08126cddabb027782ed2dff2f07a48b3185
Wellington Guimaraes <wellguimaraes@gmail.com>
2021-10-19 10:04:36 -0300

    Improved error messages

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Sync-env
-A CLI tool to easily define environment variables based on a template file.
+A CLI tool to easily define environment variables based on a template file (`.env.template` or `.env.example`).
 
 - When running **locally** it will ask for the values of missing variables
 - When running **on CI**, it will break the process if some variable is missing
--- index.js
+++ index.js
@@ -4,41 +4,54 @@ const dotEnv = require("dotenv")
 const fs = require("fs")
 const path = require("path")
 const inquirer = require("inquirer")
+const isCI = require("is-ci")
 
 dotEnv.config()
 
-const resolve = it => path.resolve(process.cwd(), it)
+const resolve = (it) => path.resolve(process.cwd(), it)
 
 const envPath = resolve(".env")
-const envTemplatePath = resolve(".env.template")
-const isCI = Object.keys(process.env).some(it => it.startsWith("CI"))
 
 module.exports = (async () => {
   const currentEnvFile = readTextFile(envPath)
-  const envTemplate = dotEnv.parse(readTextFile(envTemplatePath))
+  const envTemplate = dotEnv.parse(
+    readTextFile(resolve(".env.template")) ||
+      readTextFile(resolve(".env.example"))
+  )
+
   const currentEnv = dotEnv.parse(currentEnvFile)
   const missingEnvVariables = Object.keys(envTemplate)
-    .filter(it => !process.env[it])
-    .map(key => ({ key, value: envTemplate[key] }))
+    .filter((it) => !process.env[it])
+    .map((key) => ({ key, value: envTemplate[key] }))
 
-  if (missingEnvVariables.some(it => !it.value) && !isCI) {
-    console.log("\nOops! Some environment variables are missing...\n")
+  const missingEnvVariablesWithoutDefaultValue = missingEnvVariables.filter(
+    (it) => !it.value
+  )
+
+  if (missingEnvVariablesWithoutDefaultValue.length) {
+    if (isCI) {
+      console.error(`\n\n❌  Error: The following env variables are missing:`)
+      missingEnvVariablesWithoutDefaultValue.forEach((it) =>
+        console.error(` - ${it.key}`)
+      )
+      console.error("\n\n")
+      process.exit(1)
+    } else {
+      console.log("\n❌  Oops! Some environment variables are missing...\n")
+    }
   }
 
   for (const { key, value } of missingEnvVariables) {
     if (value) {
       currentEnv[key] = value
-    } else if (isCI) {
-      console.error(`\n\nError: The environment variable ${key} is missing\n\n`)
-      process.exit(1)
     } else {
       const { newValue } = await inquirer.prompt([
         {
           type: "input",
           name: "newValue",
           message: `What should be the value of ${key}?`,
-          validate: Boolean
-        }
+          validate: Boolean,
+        },
       ])
 
       currentEnv[key] = newValue
